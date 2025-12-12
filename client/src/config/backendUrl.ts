d6cb7ce1f6c55e38e766cd400083e3b1795443c4
Ankit27-09 <ankit27092004@gmail.com>
2025-12-12 14:47:46 +0530

    add axios instance with request and response interceptors for token management

---
+++ client/src/config/backendUrl.ts
@@ -0,0 +1,3 @@
+export const backendUrl = 'http://localhost:5050';
+// export const backendUrlPython = 'https://raven-casual-hardly.ngrok-free.app';
+// export const webSocketurl = 'wss://raven-casual-hardly.ngrok-free.app';
