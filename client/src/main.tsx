b2f7fc9fd7573b86b4ff1c275ea0f5ed8cc06b34
Ankit27-09 <ankit27092004@gmail.com>
2025-12-12 14:44:07 +0530

    replace App component with RouterProvider and Redux Provider for routing and state management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {  RouterProvider } from 'react-router-dom'
+import router from './router.tsx'
+import { Provider } from "react-redux";
+import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
+);
